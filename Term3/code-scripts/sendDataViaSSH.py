712aba12e89b71de33b694532f203b96cec0a0ff
JAMota <joao.andre.mota@gmail.com>
2023-06-14 00:39:10 +0100

    first line debug

--- Term3/code-scripts/sendDataViaSSH.py
+++ Term3/code-scripts/sendDataViaSSH.py
@@ -4,32 +4,41 @@ Created on Tue Jun 13 01:36:26 2023
 
 @author: AndreMota
 """
-
+import logging
 from time import sleep
-from confluent_kafka import Producer
 from sshtunnel import SSHTunnelForwarder
+from confluent_kafka import Producer
 
 def send_line_to_topic(line, topic, producer):
-    producer.produce(topic, line.encode('utf-8'))
-    producer.flush()
+    try:
+        logging.info("Sending line to Kafka: %s", line)
+        producer.produce(topic, line.encode('utf-8'))
+        producer.flush()
+        logging.info("Line sent to Kafka: %s", line)
+    except Exception as e:
+        logging.error("Error while sending line to Kafka: %s", str(e))
 
 def read_file_and_send_to_kafka(file_path, topic, ssh_host, ssh_port, ssh_username, ssh_private_key, kafka_bootstrap_servers):
-    with SSHTunnelForwarder(
-            (ssh_host, ssh_port),
-            ssh_username=ssh_username,
-            ssh_pkey=ssh_private_key,
-            remote_bind_address=('localhost', 9092)
-    ) as tunnel:
-        conf = {'bootstrap.servers': kafka_bootstrap_servers}
-        producer = Producer(conf)
+    try:
+        with SSHTunnelForwarder(
+                (ssh_host, ssh_port),
+                ssh_username=ssh_username,
+                ssh_pkey=ssh_private_key,
+                remote_bind_address=('localhost', 9092)
+        ) as _:
+            conf = {'bootstrap.servers': kafka_bootstrap_servers}
+            producer = Producer(conf)
 
-        with open(file_path, 'r') as file:
-            for line in file:
-                line = line.strip()
-                send_line_to_topic(line, topic, producer)
-                sleep(1)  # Wait for 1 second before sending the next line
+            with open(file_path, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    send_line_to_topic(line, topic, producer)
+                    sleep(1)  # Wait for 1 second before sending the next line
 
-        producer.flush()
+            producer.flush()
+
+    except Exception as e:
+        logging.error("Error while reading file and sending data to Kafka: %s", str(e))
 
 # SSH tunnel configuration
 ssh_host = '20.90.165.83'
@@ -40,6 +49,9 @@ ssh_private_key = "C:/Users/AndreMota/Downloads/UbuntuApacheKofta_key.pem"
 # Kafka configuration
 kafka_bootstrap_servers = 'localhost:9092'
 topic = 'heart-data'
-file_path = "C:/AppliedDataScienceAndStatistics/Applied-Data-Science-and-Statistics/Term3/heartData/b1.txt" 
+file_path = "C:/AppliedDataScienceAndStatistics/Applied-Data-Science-and-Statistics/Term3/heartData/b1.txt"
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 read_file_and_send_to_kafka(file_path, topic, ssh_host, ssh_port, ssh_username, ssh_private_key, kafka_bootstrap_servers)
