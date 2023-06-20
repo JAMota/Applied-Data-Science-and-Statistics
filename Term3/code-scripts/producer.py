0b62e1d8169821c4580ff5261f5a7a1c1ea6db15
JAMota <joao.andre.mota@gmail.com>
2023-06-20 11:29:26 +0100

    Missing the producer script

---
+++ Term3/code-scripts/producer.py
@@ -0,0 +1,30 @@
+from time import sleep
+from confluent_kafka import Producer
+
+
+def send_line_to_topic(line, topic, producer):
+    producer.produce(topic, line.encode('utf-8'))
+    producer.flush()
+
+
+def read_file_and_send_to_kafka(file_path, topic, bootstrap_servers):
+    conf = {'bootstrap.servers': bootstrap_servers}
+    producer = Producer(conf)
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            send_line_to_topic(line, topic, producer)
+            sleep(1)  # Wait for 1 second before sending the next line
+
+    producer.flush()
+
+
+# Usage example
+file_path =  "b1.txt" #"C:/AppliedDataScienceAndStatistics/Applied-Data-Science-and-Statistics/Term3/heartData/b1.txt"
+##'path/to/your/file.txt'
+topic = 'heart-data'  # 'your-kafka-topic'
+bootstrap_servers = '10.0.0.4:9092'
+#'localhost:9092'  # Update with your Kafka broker addresses
+
+read_file_and_send_to_kafka(file_path, topic, bootstrap_servers)
