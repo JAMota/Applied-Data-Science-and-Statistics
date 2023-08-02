6d74f97b30626f5b95732dc291ffd3c2ebec910e
JAMota <joao.andre.mota@gmail.com>
2023-08-02 18:48:59 +0100

    Consumer script fix

--- Term3/code-scripts/consumer.py
+++ Term3/code-scripts/consumer.py
@@ -12,43 +12,73 @@ consumer = Consumer(conf)
 
 def consume_messages():
     try:
+        consumer.subscribe(['heart-data']) #insert your topic name here
+        conn = sqlite3.connect('/opt/sqlite3/heart.db') #insert your database path here
+        cursor = conn.cursor()
+        timestamp_file = open('timestamps.log', 'a')
 
         while True:
-       
+            msg = consumer.poll(1.0)  
+            if msg is None:
+                continue
+            if msg.error():
+                raise KafkaException(msg.error())
+            
+            kafka_entry_timestamp = msg.timestamp()[1]           # kafka_entry_timestamp
+            consumer_received_timestamp = str(time.time())    # consumer_received_timestamp
 
-            # Insert the message values and timestamps into the database
+            headers = msg.headers()
+            producer_entry_timestamp = None
+            producer_sent_timestamp = None
+
+            if headers is not None:
+                for header in headers:
+                    header_key = header[0]
+                    header_value = header[1]
+                    
+                    if header_key == 'producer_entry_timestamp':
+                        producer_entry_timestamp = header_value.decode('utf-8')
+                    
+                    if header_key == 'producer_sent_timestamp':
+                        producer_sent_timestamp = header_value.decode('utf-8')
+
+            if producer_entry_timestamp and producer_sent_timestamp:
+                print(f"Producer entry timestamp: {producer_entry_timestamp}")
+                print(f"Producer sent timestamp: {producer_sent_timestamp}")
+
             values = msg.value().decode('utf-8').split()
             values.append(producer_entry_timestamp)
             values.append(producer_sent_timestamp)
-            values.append(kafka_entry_timestamp)
-            values.append(consumer_received_timestamp)
             
             database_entry_timestamp = time.time()  # Time before start of DB operation
             
             query = """
-            INSERT INTO messages (
+            INSERT INTO heart_messages (
                 heart_rate, 
                 chest_volume, 
                 blood_oxygen_concentration, 
                 producer_entry_timestamp, 
                 producer_sent_timestamp, 
-                kafka_entry_timestamp,
-                consumer_received_timestamp,
+                kafka_entry_timestamp, 
+                consumer_received_timestamp, 
                 database_entry_timestamp
-            )
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
             """
+            
             cursor.execute(query, (*values, kafka_entry_timestamp, consumer_received_timestamp, database_entry_timestamp))
             conn.commit()
-
+            
             database_finished_timestamp = time.time()  # Time after end of DB operation
-            consumer_sent_timestamp = time.ctime(time.time())  # New timestamp for consumer_sent_timestamp
+            consumer_sent_timestamp = str(time.time())     # consumer_sent_timestamp
 
-            timestamp_file.write(f"{producer_entry_timestamp} {producer_sent_timestamp} {kafka_entry_timestamp} {consumer_received_timestamp} {database_entry_timestamp} {database_finished_timestamp} {consumer_sent_timestamp}\n")
+            timestamp_file.write(
+                f"{producer_entry_timestamp} {producer_sent_timestamp} " \
+                f"{kafka_entry_timestamp} {consumer_received_timestamp} " \
+                f"{database_entry_timestamp} {database_finished_timestamp} {consumer_sent_timestamp}\n"
+            )
             timestamp_file.flush()
 
             print(f"Received message: {msg.value().decode('utf-8')}")
-
 
     except KeyboardInterrupt:
         pass
@@ -56,6 +86,5 @@ def consume_messages():
         consumer.close()
         conn.close()
         timestamp_file.close()
-
 
 consume_messages()
